01a88a1df94de29c54527d553d73b69006a2bc79
Paul_Beckwith <303dog@gmail.com>
2020-12-10 15:38:30 -0700

    commit tree d44e54183ee5f50c9889a3328db6c3b9f0fc4a08

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturdday, I want to ${activity}!`
+};
+
+function saturdayFun() {
+    return `I want to ${activity}`
+}
